3377b946c0c69abc00d651afa68c32548fc38160
Excel Dev Studio <161397288+ExcelDevStudio@users.noreply.github.com>
2024-02-27 19:48:51 -0500

    Add files via upload

---
+++ Section_3/ExcelAddInDemo1/ExcelAddInDemo1/ThisAddIn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
+
+namespace ExcelAddInDemo1
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.WorkbookNewSheet += Application_WorkbookNewSheet;
+        }
+
+        private void Application_WorkbookNewSheet(Excel.Workbook Wb, object Sh)
+        {
+            var newSht = Sh as Excel.Worksheet;
+            MessageBox.Show(string.Format("Created new sheet: {0} ", newSht.Name));
+
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
---
+++ Section_3/ExcelWorkbookDemo1/ExcelWorkbookDemo1/ThisWorkbook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace ExcelWorkbookDemo1
+{
+    public partial class ThisWorkbook
+    {
+        private void ThisWorkbook_Startup(object sender, System.EventArgs e)
+        {
+
+            // (1) Using thisworkbook and casting
+            //var sht = (this.Worksheets["Sheet1"]) as Excel.Worksheet;
+
+            // (2) Using Globals object
+            //var sht = Globals.Sheet1;
+
+            //sht.Range["F5"].Value2 = DateTime.Now;
+        }
+
+        private void ThisWorkbook_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisWorkbook_Startup);
+            this.Shutdown += new System.EventHandler(ThisWorkbook_Shutdown);
+        }
+
+        #endregion
+
+    }
+}
